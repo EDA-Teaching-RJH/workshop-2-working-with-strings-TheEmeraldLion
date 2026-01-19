79f4da7e28b7cf0809d0d070de5a7b862062ab74
TheEmeraldLion <rhf4@kent.ac.uk>
2026-01-19 14:31:50 +0000

    Part 1 Complete

--- partOne.py
+++ partOne.py
@@ -1,8 +1,10 @@
 def main():
     slow = input("Input ")
-    myFunction(slow)
+    print(myFunction(slow))
 
 def myFunction(text):
   #Your code goes here.
+  output = text.replace(" ","...")
+  return output
 
 main()
